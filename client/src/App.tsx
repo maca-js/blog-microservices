66bccdea92f8b6705c0ff50f8790a08e5826438e
Maksym Tkachuk <extmtk@anthillagency.com>
2023-02-19 14:24:53 +0200

    client: rename component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Container from '@mui/material/Container'
 import PostCreate from './components/PostCreate'
-import PostList from './components/PostList'
+import PostList from './components/PostList_hotfix'
 
 function App() {
   return (
