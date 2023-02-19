dc9bf63ef5d203459a89c7197346d7cd102322bf
Maksym Tkachuk <extmtk@anthillagency.com>
2023-02-19 19:56:05 +0200

    comments: handle CommentModerated event

--- comments/index.js
+++ comments/index.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 const commentsByPostId = {}
+const defaultStatus = 'pending'
 
 app.get('/posts/:id/comments', (req, res) => {
   res.send(commentsByPostId[req.params.id] || [])
@@ -19,7 +20,7 @@ app.post('/posts/:id/comments', async (req, res) => {
   const { content } = req.body
 
   const comments = commentsByPostId[req.params.id] || []
-  comments.push({ id: commentId, content })
+  comments.push({ id: commentId, content, status: defaultStatus })
 
   commentsByPostId[req.params.id] = comments
 
@@ -29,14 +30,30 @@ app.post('/posts/:id/comments', async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: defaultStatus,
     },
   })
 
   res.status(201).send(comments)
 })
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
   console.log('Received Event:', req.body.type)
+
+  const { type, data } = req.body
+
+  if (type === 'CommentModerated') {
+    const { id, content, postId, status } = data
+
+    const comments = commentsByPostId[postId]
+    const comment = comments.find((comment) => comment.id === id)
+    comment.status = status
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentUpdated',
+      data: { id, content, postId, status },
+    })
+  }
 
   res.send({})
 })
